58b0ce8651f54c90c565129de88d0ba581cfd1be
patrick <patricklindner@outlook.com>
2023-03-02 19:52:21 +0100

    added comments to scripts

--- dump_db.sh
+++ dump_db.sh
@@ -1,3 +1,5 @@
 docker exec pr_sentiment_mongo mongoexport --username=root --password=s3cret --authenticationDatabase=admin --db=pull-requests-raw --collection=tensorflow --out=/tmp/dumps/raw/tensorflow.json
+# add more collections here
+
 
 #docker exec pr_sentiment_mongo mongoexport --username=root --password=s3cret --authenticationDatabase=admin --db=pull-requests-raw --collection=processed --out=/tmp/dumps/processed/tensorflow.json
--- src/test.py
+++
@@ -1,6 +0,0 @@
-from time import sleep
-
-
-for i in range(1, 100):
-    progress_bar(i, 100)
-    sleep(0.5)
