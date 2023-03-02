6fda30ed620ded47cff7b6960027ade37149d56b
patrick <patricklindner@outlook.com>
2023-03-02 19:52:37 +0100

    added comments to scripts

--- load_db.sh
+++ load_db.sh
@@ -1,4 +1,4 @@
 docker exec pr_sentiment_mongo mongoimport --username=root --password=s3cret --authenticationDatabase=admin --db=pull-requests-raw --collection=tensorflow --file=/tmp/dumps/raw/tensorflow.json
-
+# add more collections here
 
 #docker exec pr_sentiment_mongo mongoimport --username=root --password=s3cret --authenticationDatabase=admin --db=pull-requests --collection=processed --file=/tmp/dumps/processed.json
