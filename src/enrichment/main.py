26e48ffb4aad43ad2e2c7d3e05285ab7a868ad0d
AntoninThioux <antonin.thioux@gmail.com>
2023-03-16 00:17:58 +0100

    Started on async

--- src/enrichment/main.py
+++ src/enrichment/main.py
@@ -1,14 +1,23 @@
 """
-This file contains the logic for enriching the pull request data in the dataset.
+This file contains the logic for enriching the pull request data in the
+dataset.
 """
 
 from src.util.mongo import get_database
 from src.util.ProjectListReader import ProjectListReader
-import pymongo
+from src.enrichment.token import sleeper
+from src.enrichment.enricher import enrich_pull_request
+
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from pymongo.collection import Collection
 
 DB_NAME = 'pull-requests-raw'
 REPO_FILE_PATH = "../resources/project-list.txt"
 RESOLUTION = 100
+BATCH_SIZE = 40
+MAX_WORKERS = 20
+REQUESTS_PER_PR = 2
 
 
 def main():
@@ -20,19 +29,40 @@ def main():
         enrich_repository_collection(repository_collection)
 
 
-def enrich_repository_collection(repository_collection):
-    user_contributions = dict()
-
-    query = repository_collection.find({}).sort('_id', pymongo.DESCENDING)
-    for row in query:
-        if row['merged_at'] is None:
-            continue
-        update_contributions(user_contributions, row)
+@sleeper(number_of_requests=(REQUESTS_PER_PR * BATCH_SIZE))
+def async_batch(batch: list, repository_collection: Collection):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        enrich_batch(batch, repository_collection)
+    )
 
 
-def update_contributions(user_contributions, row):
-    author = row['user_id']
-    user_contributions[author] = user_contributions.get(author, 0) + 1
+async def enrich_batch(batch: list, repository_collection: Collection):
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        loop = asyncio.get_event_loop()
+        futures = [
+            loop.run_in_executor(executor, enrich_pull_request, batch[i])
+            for i in range(BATCH_SIZE)
+        ]
+
+        for enriched_pull_request in await asyncio.gather(*futures):
+            update_pull_request(enriched_pull_request, repository_collection)
+
+
+def update_pull_request(pull_request: dict, repository_collection: Collection):
+    pull_request_id = pull_request['_id']
+    repository_collection.replace_one(
+        {'_id': pull_request_id},
+        pull_request
+    )
+
+
+def enrich_repository_collection(repository_collection: Collection):
+    size = repository_collection.find().count()
+    for i in range(0, size, BATCH_SIZE):
+        batch_query = repository_collection.find({}).skip(i).limit(size)
+        batch = list(batch_query)
+        async_batch(batch, repository_collection)
 
 
 if __name__ == '__main__':
