20691f86e264e0fc32e7f7737fe7f3629a39d9dc
AntoninThioux <antonin.thioux@gmail.com>
2023-03-26 21:28:39 +0200

    More cleaning on sentiments

--- src/cleaning/main.py
+++ src/cleaning/main.py
@@ -1,10 +1,15 @@
 """
 This file contains the logic for cleaning the data in the dataset.
 
-The cleaning is done based on two criteria:
+The cleaning is done based on five criteria:
  1. Has the pull request been merged? No? Then remove!
- 2. Has the pull request been closed in less than 5 minutes? Yes then it is
+ 2. The pull request has a body text description.
+ 3. Has the pull request been closed in less than 5 minutes? Yes then it is
     likely reviewed offline or not at all. Remove.
+ 4. Are there comments? No? Then remove!
+ 5. Are is there interaction between author and reviewer(s) in the comments?
+    Have both the author and the reviewer(s) made comments? No, then maybe it
+    isn't a review. Remove.
 """
 
 from src.util.mongo import get_database
@@ -14,9 +19,9 @@ from src.helpers.loading_bar import print_loading_bar, print_finished_load_bar
 from dateutil import parser
 from pymongo.collection import Collection
 
-DB_NAME_RAW = 'pull-requests-raw'
-DB_NAME_CLEAN = 'pull-requests-clean'
-REPO_FILE_PATH = "../resources/project-list.txt"
+DB_NAME_RAW = 'pull-requests-sentiment'
+DB_NAME_CLEAN = 'pull-requests-sentiment-clean'
+REPO_FILE_PATH = "./resources/project-list.txt"
 
 
 def main():
@@ -35,9 +40,24 @@ def is_dirty_row(row: dict):
     if not row['merged_at']:
         return True
 
+    if not row['body']:
+        return True
+
     created_time = parser.parse(row['created_at'])
     merged_time = parser.parse(row['merged_at'])
     if (merged_time - created_time).seconds < 300:
+        return True
+
+    if 'comments_participants' not in row.keys():
+        return False
+
+    if row.get('comments_count') == 0:
+        return True
+
+    if 'author_comment_average' not in row.keys():
+        return False
+
+    if not row['author_comment_average'] or not row['review_comment_average']:
         return True
 
     return False
